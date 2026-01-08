a56de4844cc0f7cf307c04dabb5e94a36a231378
Rowshnick <127508073+Rowshnick@users.noreply.github.com>
2026-01-08 13:30:45 +0330

    Update start_menu.py

--- keyboards/start_menu.py
+++ keyboards/start_menu.py
@@ -1,19 +1,16 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-
 def start_main_menu():
     return InlineKeyboardMarkup(
         inline_keyboard=[
-            [InlineKeyboardButton(text="🔮 گزارش ناتال", callback_data="start_natal")],
-            [InlineKeyboardButton(text="📜 ترانزیت‌ها", callback_data="start_transits")],
-            [InlineKeyboardButton(text="✨ Symbol Menu", callback_data="start_symbol")],
-        ]
-    )
-
-
-def back_to_main_menu():
-    return InlineKeyboardMarkup(
-        inline_keyboard=[
-            [InlineKeyboardButton(text="🔙 بازگشت به منوی اصلی", callback_data="back_main")]
+            [
+                InlineKeyboardButton(text="🔮 چارت تولد", callback_data="natal_chart")
+            ],
+            [
+                InlineKeyboardButton(text="♓️ نمادها", callback_data="symbols")
+            ],
+            [
+                InlineKeyboardButton(text="🪐 ترانزیت‌ها", callback_data="transits")
+            ]
         ]
     )
