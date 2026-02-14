49745d5991b44cdb72eabbbba3eda593dd7cad59
Rowshnick <127508073+Rowshnick@users.noreply.github.com>
2026-02-14 16:38:35 +0330

    Initialize sajil module with imports and exports

--- sajil/__init__.py
+++ sajil/__init__.py
@@ -1,1 +1,12 @@
+# sajil/__init__.py
 
+from .models import SajilInput, SajilResult
+from .engine import compute_sajil
+from .formatter import format_sajil_text
+
+__all__ = [
+    "SajilInput",
+    "SajilResult",
+    "compute_sajil",
+    "format_sajil_text",
+]
