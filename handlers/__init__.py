20a59fc7bc18ba54e21d63b2d37087bf3c28db1a
Rowshnick <127508073+Rowshnick@users.noreply.github.com>
2026-02-15 10:45:59 +0330

    Add header comment to handlers/__init__.py
    Add a header comment to the init file

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,3 +1,5 @@
+# handlers/__init__.py
+
 from .start import router as start_router
 from .symbol_handlers import router as symbol_handlers_router
 from .symbol_inline import router as symbol_inline_router
