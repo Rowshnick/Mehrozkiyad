89694532537b00e5d115d2b01a33bccf6adbc380
Rowshnick <127508073+Rowshnick@users.noreply.github.com>
2026-02-15 19:44:27 +0330

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,17 +1,17 @@
 # handlers/__init__.py
 
 from .start import router as start_router
-from .symbol_handlers import router as symbol_handlers_router
+from .sajil_handlers import router as sajil_router
+from .symbol_handlers import router as symbol_router
 from .symbol_inline import router as symbol_inline_router
-from .transits import router as transits_router
 from .natal_fsm import router as natal_router
-from .sajil_handlers import router as sajil_handlers_router
+from .transits import router as transits_router
 
 all_routers = [
     start_router,
-    symbol_handlers_router,
+    sajil_router,
+    symbol_router,
     symbol_inline_router,
-    transits_router,
     natal_router,
-    sajil_handlers_router,
+    transits_router,
 ]
